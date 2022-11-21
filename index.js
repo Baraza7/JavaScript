a355b3ead743b39091a4725c8e0e25dded85be1a
baraza7 <barazabrian87@gmail.com>
2022-11-21 16:15:26 +0300

    Fist JavaScript Lecture

---
+++ index.js
@@ -0,0 +1,16 @@
+//Declaring a variable => Keyword <variableName> = <value>
+//single line commenting
+/*
+multi-line comments
+*/
+
+//let_number_of_students = 50 //snake_case
+//letNumberOfStudents = 50 //camelCase
+
+let numberOfStudents = 100
+
+const numberOfStudents = 200
+
+
+let areaTri = base * height
+const base =
